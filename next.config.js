224bb08fc592e00bd48e30bb3181bb41f6101f59
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-10-31 23:51:41 +0530

    Chingari page and API integrated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["i.ytimg.com", "i0.wp.com", "aiovideodl.ml", "f4.bcbits.com", "scontent.fstv5-1.fna.fbcdn.net", "cdn2.sharechat.com"],
+    domains: ["i.ytimg.com", "i0.wp.com", "aiovideodl.ml", "f4.bcbits.com", "scontent.fstv5-1.fna.fbcdn.net", "cdn2.sharechat.com", "media.chingari.io"],
   },
 };
 
